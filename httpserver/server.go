e9c10e78942335c5b6b700d4a9c695f381db44dc
Ramon Moraes <ramon.moraes94@gmail.com>
2019-12-23 16:47:33 -0300

    add httpserver module

---
+++ httpserver/server.go
@@ -0,0 +1,34 @@
+package httpserver
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func Serve() {
+	http.HandleFunc("/", jsonUnpackHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+type withName struct {
+	Name string
+}
+
+func jsonUnpackHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var jsonRequest withName
+	err = json.Unmarshal(body, &jsonRequest)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(w, jsonRequest.Name)
+}
